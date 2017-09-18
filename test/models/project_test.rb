f341f465bda49c1cb4e1143c59512c86cedcdea9
tenzin2017 <tenzinleo@gmail.com>
2017-09-18 17:03:22 -0400

    validations of projects start_date, end_date, goal are created and tested successfully

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-
+require 'pry'
 class ProjectTest < ActiveSupport::TestCase
 
   test 'valid project can be created' do
@@ -20,6 +20,42 @@ class ProjectTest < ActiveSupport::TestCase
     assert project.invalid?, 'Project should not save without owner.'
   end
 
+  test 'Project goal must be positive' do
+    owner = new_user
+    owner.save
+    project = new_project
+    project.user = owner
+    project.goal = 1
+    project.save
+    assert project.valid?
+    assert project.persisted?
+    assert project.user
+  end
+
+  test 'Project start_date must be in future' do
+    owner = new_user
+    owner.save
+    project = new_project
+    project.user = owner
+    project.start_date = Date.today + 1.day
+    project.save
+    assert project.valid?
+    assert project.persisted?
+    assert project.user
+  end
+
+  test 'Project end_date must be greater than start_date' do
+    owner = new_user
+    owner.save
+    project = new_project
+    project.user = owner
+    project.start_date = Date.today + 1.day
+    project.end_date = Date.today + 2.day
+    project.save
+    assert project.valid?
+    assert project.persisted?
+    assert project.user
+  end
   def new_project
     Project.new(
       title:       'Cool new boardgame',
