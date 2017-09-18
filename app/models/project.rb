4dc090341939e16fcd40f734c3c860bf1a5f483c
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-18 16:47:01 -0400

    added validation for Project model for:[start_date_must_be_in_future,end_date_must_be_later_than_start_date, goal greater than or equal to zero]

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,5 +7,21 @@ class Project < ActiveRecord::Base
 
   validates :title, :user_id, :description, :goal, :start_date, :end_date, presence: true
 
+  validates :goal, :numericality => {:greater_than_or_equal_to => 0}
+
+  validate :start_date_set_to_future
+  validate :end_date_later_than_start_date
+
+  def start_date_set_to_future
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "Start Date has to be future date!")
+    end
+  end
+
+  def end_date_later_than_start_date
+    if end_date.present? && end_date < start_date
+      errors.add(:end_date, "End Date has to be greater than start date!")
+    end
+  end
 
 end
