69e147fa36f7813ee4aec32b0454d8049d4c0b37
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-21 11:20:28 -0400

    fixed all validation test without any errors  using factory Girl gem

--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -4,55 +4,22 @@ require 'pry'
 
 class PledgeTest < ActiveSupport::TestCase
   test 'A pledge can be created' do
-
-    # owner = new_user
-    # owner.save
-    # project = new_project
-    # project.user = owner
-    # project.save
-    # pledgie = new_user
-    # pledgie.email = "pledgie@gmail.com"
-    # pledgie.save
-    user1 = build(:user, first_name:'Mally')
-    project1 = build(:project)
-    pledge = build(:pledge, user:user1,project:project1)
-
-    pledge.save
+    user1 = create(:user, first_name:'Tally')
+    user2 = create(:user, first_name:'Mally')
+    project1 = create(:project)
+    pledge = build(:pledge, user:user2,project:project1)
     assert pledge.valid?
-    # assert pledge.persisted?
   end
 
   test 'owner cannot back own project' do
-    owner = new_user
-    owner.save
-    project = new_project
-    project.user = owner
-    project.save
-    pledge = build(:pledge,project: project)
-    pledge.user = owner
-    pledge.save
-    assert pledge.invalid?, 'Owner should not be able to pledge towards own project'
+    user1 = create(:user, first_name:'Tally')
+    user2 = create(:user, first_name:'Mally')
+    project1 = create(:project, user: user1)
+    pledge = build(:pledge, user:user1,project:project1)
+
+    refute pledge.valid?
 
   end
 
-  def new_project
-    Project.new(
-      title:       'Cool new boardgame',
-      description: 'Trade sheep',
-      start_date:  Date.today,
-      end_date:    Date.today + 1.month,
-      goal:        50000
-    )
-  end
-
-  def new_user
-    User.new(
-      first_name:            'Sally',
-      last_name:             'Lowenthal',
-      email:                 'sally@example.com',
-      password:              'passpass',
-      password_confirmation: 'passpass'
-    )
-  end
 
 end
